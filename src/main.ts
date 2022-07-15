51a047549c09df38ebb33804d004f45d72631aa1
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-07-15 13:29:47 +0500

    Add users module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
+import { parse } from 'yaml';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // Parse api file from provided document
+  const apiFile = readFileSync(
+    resolve(process.cwd(), './doc/api.yaml'),
+    'utf-8',
+  );
+  const schema = parse(apiFile);
+
+  SwaggerModule.setup('api', app, schema);
+
   await app.listen(4000);
 }
 bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,89 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  HttpStatus,
+  Res,
+  Body,
+  Param,
+  NotFoundException,
+  BadRequestException,
+  ForbiddenException,
+  HttpCode,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { validate as uuidValidate } from 'uuid';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { UsersService } from './services/users.service';
+
+@Controller({ path: 'users' })
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid user id');
+    }
+
+    const user = this.usersService.findOneById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    const newUser = this.usersService.create(createUserDto);
+    return res.status(HttpStatus.CREATED).send(newUser);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid user id');
+    }
+
+    const { record, successfullyUpdated } = this.usersService.updatePassword(
+      id,
+      updatePasswordDto,
+    );
+
+    if (!record) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (record && !successfullyUpdated) {
+      throw new ForbiddenException('oldPassword is wrong');
+    }
+
+    return record;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id') id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('Invalid user id');
+    }
+
+    const result = this.usersService.delete(id);
+
+    if (!result) {
+      throw new NotFoundException('User not found');
+    }
+  }
+}
