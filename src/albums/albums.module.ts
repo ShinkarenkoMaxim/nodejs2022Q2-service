824d33ccade2c889fcec4bcbd861a0fc779f5bc9
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-07-16 22:05:02 +0500

    Fix provider

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArtistsService } from 'src/artists/services/artists.service';
 import { InMemoryDBService } from 'src/providers/database/inmemory/inmemory-db.service';
+import { TracksService } from 'src/tracks/services/tracks.service';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './services/albums.service';
 
 @Module({
   controllers: [AlbumsController],
-  providers: [AlbumsService, ArtistsService, InMemoryDBService],
+  providers: [AlbumsService, ArtistsService, TracksService, InMemoryDBService],
 })
 export class AlbumsModule {}
