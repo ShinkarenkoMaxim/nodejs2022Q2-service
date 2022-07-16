a2e50ca18af65fe21a6eb73d79c10abd5d6ad9ff
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-07-17 02:09:30 +0500

    Fix import

--- src/tracks/tracks.module.ts
+++ src/tracks/tracks.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AlbumsService } from 'src/albums/services/albums.service';
-import { ArtistsService } from 'src/artists/services/artists.service';
 import { FavoritesService } from 'src/favorites/services/favorites.service';
 import { InMemoryDBService } from 'src/providers/database/inmemory/inmemory-db.service';
 import { TracksService } from './services/tracks.service';
